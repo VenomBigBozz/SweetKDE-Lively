319657ba949ebce806e1b6dfd6cb6bcc95f5d89a
Riccardo Nuncibello <32012346+VenomBigBozz@users.noreply.github.com>
2022-01-24 20:01:13 +0100

    clean code

--- outerSpace.js
+++ outerSpace.js
@@ -1,334 +1,127 @@
-function init() {
-    particlesJS("particles-js", {
-        particles: {
-            number: {
-                value: 150,
-                density: {
-                    enable: true,
-                    value_area: 800,
-                },
-            },
-            color: {
-                value: "#ffffff",
-            },
-            shape: {
-                type: "circle",
-                stroke: {
-                    width: 0,
-                    color: "#000000",
-                },
-                polygon: {
-                    nb_sides: 5,
-                },
-                image: {
-                    src: "img/github.svg",
-                    width: 100,
-                    height: 100,
-                },
-            },
-            opacity: {
-                value: 0.8,
-                random: true,
-                anim: {
-                    enable: true,
-                    speed: 1,
-                    opacity_min: 0,
-                    sync: false,
-                },
-            },
-            size: {
-                value: 2,
-                random: true,
-                anim: {
-                    enable: true,
-                    speed: 2,
-                    size_min: 0.3,
-                    sync: false,
-                },
-            },
-            line_linked: {
-                enable: false,
-                distance: 150,
-                color: "#ffffff",
-                opacity: 0.4,
-                width: 1,
-            },
-            move: {
+function initParallax() {
+    particlesJS("particles-js", ParticlesConfig);
+    particlesJS("particles-js2", ParticlesConfig);
+    particlesJS("particles-js3", ParticlesConfig);
+}
+window.addEventListener('DOMContentLoaded', (event) => {
+    var scene = document.getElementById('scene');
+    var parallaxInstance = new Parallax(scene);
+    initParallax();
+    window.addEventListener('resize', (event) => {
+        initParallax()
+    })
+});
+
+const ParticlesConfig = {
+    particles: {
+        number: {
+            value: 150,
+            density: {
                 enable: true,
-                speed: 0.5,
-                direction: "none",
-                random: true,
-                straight: false,
-                out_mode: "out",
-                bounce: false,
-                attract: {
-                    enable: false,
-                    rotateX: 600,
-                    rotateY: 600,
-                },
+                value_area: 800,
             },
         },
-        interactivity: {
-            detect_on: "window",
-            events: {
-                onhover: {
-                    enable: true,
-                    mode: "bubble",
-                },
-                onclick: {
-                    enable: false,
-                    mode: "repulse",
-                },
-                resize: true,
+        color: {
+            value: "#ffffff",
+        },
+        shape: {
+            type: "circle",
+            stroke: {
+                width: 0,
+                color: "#000000",
             },
-            modes: {
-                grab: {
-                    distance: 400,
-                    line_linked: {
-                        opacity: 1,
-                    },
-                },
-                bubble: {
-                    distance: 131.85970127784034,
-                    size: 2.1,
-                    duration: 0,
-                    opacity: 1,
-                    speed: 3,
-                },
-                repulse: {
-                    distance: 400,
-                    duration: 0.4,
-                },
-                push: {
-                    particles_nb: 4,
-                },
-                remove: {
-                    particles_nb: 2,
-                },
+            polygon: {
+                nb_sides: 5,
+            },
+            image: {
+                src: "img/github.svg",
+                width: 100,
+                height: 100,
             },
         },
-        retina_detect: true,
-    });
-    particlesJS("particles-js2", {
-        particles: {
-            number: {
-                value: 150,
-                density: {
-                    enable: true,
-                    value_area: 800,
-                },
-            },
-            color: {
-                value: "#ffffff",
-            },
-            shape: {
-                type: "circle",
-                stroke: {
-                    width: 0,
-                    color: "#000000",
-                },
-                polygon: {
-                    nb_sides: 5,
-                },
-                image: {
-                    src: "img/github.svg",
-                    width: 100,
-                    height: 100,
-                },
-            },
-            opacity: {
-                value: 0.8,
-                random: true,
-                anim: {
-                    enable: true,
-                    speed: 1,
-                    opacity_min: 0,
-                    sync: false,
-                },
-            },
-            size: {
-                value: 2,
-                random: true,
-                anim: {
-                    enable: true,
-                    speed: 2,
-                    size_min: 0.3,
-                    sync: false,
-                },
-            },
-            line_linked: {
-                enable: false,
-                distance: 150,
-                color: "#ffffff",
-                opacity: 0.4,
-                width: 1,
-            },
-            move: {
+        opacity: {
+            value: 0.8,
+            random: true,
+            anim: {
                 enable: true,
-                speed: 0.5,
-                direction: "none",
-                random: true,
-                straight: false,
-                out_mode: "out",
-                bounce: false,
-                attract: {
-                    enable: false,
-                    rotateX: 600,
-                    rotateY: 600,
-                },
+                speed: 1,
+                opacity_min: 0,
+                sync: false,
             },
         },
-        interactivity: {
-            detect_on: "window",
-            events: {
-                onhover: {
-                    enable: true,
-                    mode: "bubble",
-                },
-                onclick: {
-                    enable: false,
-                    mode: "repulse",
-                },
-                resize: true,
-            },
-            modes: {
-                grab: {
-                    distance: 400,
-                    line_linked: {
-                        opacity: 1,
-                    },
-                },
-                bubble: {
-                    distance: 131.85970127784034,
-                    size: 2.1,
-                    duration: 0,
-                    opacity: 1,
-                    speed: 3,
-                },
-                repulse: {
-                    distance: 400,
-                    duration: 0.4,
-                },
-                push: {
-                    particles_nb: 4,
-                },
-                remove: {
-                    particles_nb: 2,
-                },
-            },
-        },
-        retina_detect: true,
-    });
-    particlesJS("particles-js3", {
-        particles: {
-            number: {
-                value: 150,
-                density: {
-                    enable: true,
-                    value_area: 800,
-                },
-            },
-            color: {
-                value: "#ffffff",
-            },
-            shape: {
-                type: "circle",
-                stroke: {
-                    width: 0,
-                    color: "#000000",
-                },
-                polygon: {
-                    nb_sides: 5,
-                },
-                image: {
-                    src: "img/github.svg",
-                    width: 100,
-                    height: 100,
-                },
-            },
-            opacity: {
-                value: 0.8,
-                random: true,
-                anim: {
-                    enable: true,
-                    speed: 1,
-                    opacity_min: 0,
-                    sync: false,
-                },
-            },
-            size: {
-                value: 2,
-                random: true,
-                anim: {
-                    enable: true,
-                    speed: 2,
-                    size_min: 0.3,
-                    sync: false,
-                },
-            },
-            line_linked: {
-                enable: false,
-                distance: 150,
-                color: "#ffffff",
-                opacity: 0.4,
-                width: 1,
-            },
-            move: {
+        size: {
+            value: 2,
+            random: true,
+            anim: {
                 enable: true,
-                speed: 0.5,
-                direction: "none",
-                random: true,
-                straight: false,
-                out_mode: "out",
-                bounce: false,
-                attract: {
-                    enable: false,
-                    rotateX: 600,
-                    rotateY: 600,
-                },
+                speed: 2,
+                size_min: 0.3,
+                sync: false,
             },
         },
-        interactivity: {
-            detect_on: "window",
-            events: {
-                onhover: {
-                    enable: true,
-                    mode: "bubble",
-                },
-                onclick: {
-                    enable: false,
-                    mode: "repulse",
-                },
-                resize: true,
+        line_linked: {
+            enable: false,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1,
+        },
+        move: {
+            enable: true,
+            speed: 0.5,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 600,
             },
-            modes: {
-                grab: {
-                    distance: 400,
-                    line_linked: {
-                        opacity: 1,
-                    },
-                },
-                bubble: {
-                    distance: 131.85970127784034,
-                    size: 2.1,
-                    duration: 0,
+        },
+    },
+    interactivity: {
+        detect_on: "window",
+        events: {
+            onhover: {
+                enable: true,
+                mode: "bubble",
+            },
+            onclick: {
+                enable: false,
+                mode: "repulse",
+            },
+            resize: true,
+        },
+        modes: {
+            grab: {
+                distance: 400,
+                line_linked: {
                     opacity: 1,
-                    speed: 3,
-                },
-                repulse: {
-                    distance: 400,
-                    duration: 0.4,
-                },
-                push: {
-                    particles_nb: 4,
-                },
-                remove: {
-                    particles_nb: 2,
                 },
             },
+            bubble: {
+                distance: 131.85970127784034,
+                size: 2.1,
+                duration: 0,
+                opacity: 1,
+                speed: 3,
+            },
+            repulse: {
+                distance: 400,
+                duration: 0.4,
+            },
+            push: {
+                particles_nb: 4,
+            },
+            remove: {
+                particles_nb: 2,
+            },
         },
-        retina_detect: true,
-    });
+    },
+    retina_detect: true,
+};
 }
 window.addEventListener('DOMContentLoaded', (event) => {
     var scene = document.getElementById('scene');
