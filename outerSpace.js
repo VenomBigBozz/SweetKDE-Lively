47a843600be13dc7e287c10e45c96651d5b7cb48
Riccardo Nuncibello <32012346+VenomBigBozz@users.noreply.github.com>
2022-02-02 21:46:14 +0100

    Clean code.

--- outerSpace.js
+++ outerSpace.js
@@ -1,57 +1,51 @@
-rect = document.querySelector('#svg').getBoundingClientRect();
-mouse = {
+let rect = document.querySelector('#svg').getBoundingClientRect();
+let mouse = {
     x: 0,
     y: 0,
     moved: false
 };
+
+let className = {
+    '.cls-2': 15,   //bottom-line
+    '.cls-3': -50,  //top-line-dx
+    '.cls-4': -90,  //top-line-sx
+    '.cls-5': 30,   //outline-circle-sx
+    '.cls-6': -25,  //outline-circle-dx
+    '.cls-7': -40,  //big-circle-sx
+    '.cls-8': 85,   //top-little-circle-sx
+    '.cls-9': -85,  //top-little-circle-dx
+    '.cls-10': -40, //big-circle-dx
+    '.cls-11': -85, //bottom-little-circle-dx
+    '.cls-12': 85,  //bottom-little-circle-sx
+    '.cls-13': 40,  //center-logo-line #1
+    '.cls-14': 45,  //center-logo-line #2
+    '.cls-15': 50,  //center-logo-line #3
+    '.cls-16': 50,  //center-logo
+    '.cls-17': -28, //top-dx-lines #1
+    '.cls-18': -32, //top-dx-lines #2
+    '.cls-19': -30, //top-dx-lines #3
+    '.cls-20': 29,  //bottom-sx-lines #1
+    '.cls-21': 30,  //bottom-sx-lines #2
+};
+
 gsap.ticker.add(() => {
     if (mouse.moved) {
-        //bottom-line
-        parallaxIt('.cls-2', 15);
-        //top-line-dx
-        parallaxIt('.cls-3', -30);
-        //top-line-sx
-        parallaxIt('.cls-4', -90);
-        //outline-circle:
-        parallaxIt('.cls-5', 30); //sx
-        parallaxIt('.cls-6', -25); //dx
-        //big-circle-sx
-        parallaxIt('.cls-7', -40);
-        //little-circles-sx
-        parallaxIt('.cls-8', 85);
-        parallaxIt('.cls-9', -85);
-        //big-circle-dx
-        parallaxIt('.cls-10', -40);
-        //little-circles-dx
-        parallaxIt('.cls-11', -85);
-        parallaxIt('.cls-12', 85);
-        //center-logo-lines
-        parallaxIt('.cls-13', 35);
-        parallaxIt('.cls-14', 40);
-        parallaxIt('.cls-15', 45);
-        //center-logo
-        parallaxIt('.cls-16', 50);
-        //top-dx-lines
-        parallaxIt('.cls-17', -28);
-        parallaxIt('.cls-18', -34);
-        parallaxIt('.cls-19', -30);
-        //bottom-sx-lines
-        parallaxIt('.cls-20', -25);
-        parallaxIt('.cls-21', -30);
+        for (let key in className) {
+            parallaxIt(key, className[key]);
+        }
     }
     mouse.moved = false;
 });
 
 function initParticlesJS(config) {
     particlesJS("particles-js", config);
-    particlesJS("Cerchio-sinistro", config);
 }
 
 function parallaxIt(target, movement) {
     gsap.to(target, {
         x: ((mouse.x - rect.width / 2) / rect.width) * movement,
         y: ((mouse.y - rect.height / 2) / rect.height) * movement,
-        duration: 0.3,
+        duration: 0.8,
     });
 }
 window.addEventListener('DOMContentLoaded', (event) => {
